410c3172ae20e69d4865f70b15174c4fae9f1db1
Romarico Vargas <romaricovargas@yahoo.com>
2025-03-25 19:13:52 +1100

    Started coding the main python file

--- speed_patrol.py
+++ speed_patrol.py
@@ -1,2 +1,28 @@
 
 import pygame
+
+#Initialize pygame
+pygame.init()
+
+#Set display surface
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 600
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Speed Patrol")
+
+#Main game loop
+running = True
+while running:
+    #Check to see if the user want to quit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+#End the game
+pygame.quit()
+
+
+
+
+
+
